156b116940b3c0a6f5ac81080763ab94b9a73c37
Santiago Botero <sanbope15@gmail.com>
2020-11-02 10:46:52 -0500

    Configurando springboot

---
+++ backend/src/test/java/botero/pelaez/santiago/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package botero.pelaez.santiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
